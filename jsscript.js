e0e325af7c86de07a640fc59b3f541e0fb910eae
CipangoArt <114663221+CipangoArt@users.noreply.github.com>
2022-11-21 00:22:25 +0000

    v1

---
+++ jsscript.js
@@ -0,0 +1,33 @@
+let name = "Romeu";
+let age = 22;
+
+console.log(name, age);
+
+let newage=age*7;
+
+let friends = ["Carolina", "Leo", "Diogo"];
+
+let romeu = 
+    {
+      Rname: name,
+      Rage: age,
+      isFunny: false
+    }
+
+let Amig0s = [
+    {
+      name: friends[0],
+      age: 20,
+      isStudent: true
+    },
+    {
+      name: friends[1],
+      age: 20,
+      isStudent: true
+    },
+    {
+      name: friends[2],
+      age: 22,
+      isStudent: false
+    }
+  ];
